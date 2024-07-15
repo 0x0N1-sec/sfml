2f8de235553e94f42e71a9bef16e44587258466d
0x0n1-sec <childresszac@gmail.com>
2024-07-15 17:39:50 -0500

    working through USACO Guide

---
+++ cpp/kattis/r2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int r1 = 0, s = 0;
+  double r2 = 0.0;
+  scanf("%d %d", &r1, &s);
+
+  r2 = (s * 2.0) - r1;
+  printf("%f", r2);
+  
+  return 0;
+}
